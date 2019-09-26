3b84df4469d10bc559d5b3785b2d608136892afa
Bachi Allamsetty <bachia@gmail.com>
2019-09-26 17:14:59 +0530

    Changed course search count from COURSE to CONTENT

--- src/app/client/src/app/modules/search/components/course-search/course-search.component.ts
+++ src/app/client/src/app/modules/search/components/course-search/course-search.component.ts
@@ -111,18 +111,14 @@ export class CourseSearchComponent implements OnInit, OnDestroy {
     }
     this.searchService.courseSearch(option)
     .subscribe(data => {
-      console.log(data.result.course,"data.result.cours");
-console.log("content list ",data.result.content);
         this.showLoader = false;
         this.facetsList = this.searchService.processFilterData(_.get(data, 'result.facets'));
         this.paginationDetails = this.paginationService.getPager(data.result.count, this.paginationDetails.currentPage,
             this.configService.appConfig.SEARCH.PAGE_LIMIT);
         const { constantData, metaData, dynamicFields } = this.configService.appConfig.CoursePageSection.course;
-        this.contentList = _.map(data.result.course, (content: any) =>
+        this.contentList = _.map(data.result.content, (content: any) =>
           this.utilService.processContent(content, constantData, dynamicFields, metaData));
-          console.log(this.contentList,"contentList");
     }, err => {
-      console.log(err,"err");
         this.showLoader = false;
         this.contentList = [];
         this.facetsList = [];
