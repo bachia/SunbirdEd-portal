c8fa979ff4d51938d7cc8f490966762bb08e061f
kiranTunerlabs <kiran@tunerlabs.com>
2020-01-31 13:57:56 +0530

    web animation error fix for safari

--- gpush.sh
+++ gpush.sh
@@ -1,5 +1,5 @@
 echo -n "Enter Commit Message: "
 read msg
 git tag -d release-1.14.0 && git push origin :release-1.14.0
-git commit -a -m "$msg" && git push origin release-1.14.0-branch
+git commit -a -m  "$msg" --no-verify && git push  origin release-1.14.0-branch --no-verify
 git tag release-1.14.0 && git push origin release-1.14.0
